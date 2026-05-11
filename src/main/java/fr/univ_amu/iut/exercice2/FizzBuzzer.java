0d708f30443f708ba7cc97a96f3d9fcee7b29ef6
Aymen13m14 <aymen.el-mestari@etu.univ-amu.fr>
2026-05-11 16:16:52 +0200

    Exo1 (#2)
    * exo 1 terminer
    
    * exo 1 terminer
    
    * exo1 dans brache exo1

--- src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
+++ src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -3,16 +3,21 @@ package fr.univ_amu.iut.exercice2;
 /**
  * Exercice 2 - FizzBuzz.
  *
- * <p>Classique des entretiens d'embauche et exercice parfait pour pratiquer la <b>triangulation</b>
- * : commencez par un retour en dur, puis généralisez dès qu'un deuxième test vous y force.
+ * <p>
+ * Classique des entretiens d'embauche et exercice parfait pour pratiquer la
+ * <b>triangulation</b>
+ * : commencez par un retour en dur, puis généralisez dès qu'un deuxième test
+ * vous y force.
  *
- * <p>Règles : pour chaque entier {@code n},
+ * <p>
+ * Règles : pour chaque entier {@code n},
  *
  * <ul>
- *   <li>si {@code n} est multiple de 3 <em>et</em> de 5, retourner {@code "FizzBuzz"}
- *   <li>sinon si {@code n} est multiple de 3, retourner {@code "Fizz"}
- *   <li>sinon si {@code n} est multiple de 5, retourner {@code "Buzz"}
- *   <li>sinon retourner la représentation textuelle de {@code n}
+ * <li>si {@code n} est multiple de 3 <em>et</em> de 5, retourner
+ * {@code "FizzBuzz"}
+ * <li>sinon si {@code n} est multiple de 3, retourner {@code "Fizz"}
+ * <li>sinon si {@code n} est multiple de 5, retourner {@code "Buzz"}
+ * <li>sinon retourner la représentation textuelle de {@code n}
  * </ul>
  */
 public class FizzBuzzer {
@@ -24,10 +29,19 @@ public class FizzBuzzer {
    * @return la chaîne FizzBuzz correspondante
    */
   public String fizzBuzz(int n) {
-    // TODO exercice 2 : ajouter les cas Fizz, Buzz et FizzBuzz avant le
-    // return par défaut. Activez les tests dans l'ordre : 1 et 2 passent
-    // directement (fake it via le return final), 3 demande d'introduire
-    // un premier cas, etc.
+
+    if (n % 15 == 0) {
+      return "FizzBuzz";
+    }
+
+    if (n % 3 == 0) {
+      return "Fizz";
+    }
+
+    if (n % 5 == 0) {
+      return "Buzz";
+    }
+
     return String.valueOf(n);
   }
 
@@ -37,10 +51,15 @@ public class FizzBuzzer {
    * @param n taille de la séquence demandée (positif)
    * @return tableau de {@code n} chaînes FizzBuzz dans l'ordre croissant
    */
-  public String[] fizzBuzzJusquA(int n) {
+  public String[] fizzBuzzJusqua(int n) {
+
     String[] sequence = new String[n];
-    // TODO exercice 2 : remplir sequence[i] en réutilisant fizzBuzz(i+1).
-    // Ne dupliquez pas la logique : appelez fizzBuzz !
+
+    for (int i = 0; i < n; i++) {
+        sequence[i] = fizzBuzz(i + 1);
+    }
+
     return sequence;
+}
   }
 }
