590dc179e733bb0bab575604aa0544a3d9ff88e8
Aymen13m14 <aymen.el-mestari@etu.univ-amu.fr>
2026-05-11 14:13:01 +0000

    exo2 fini

--- src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
+++ src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -3,21 +3,16 @@ package fr.univ_amu.iut.exercice2;
 /**
  * Exercice 2 - FizzBuzz.
  *
- * <p>
- * Classique des entretiens d'embauche et exercice parfait pour pratiquer la
- * <b>triangulation</b>
- * : commencez par un retour en dur, puis généralisez dès qu'un deuxième test
- * vous y force.
+ * <p>Classique des entretiens d'embauche et exercice parfait pour pratiquer la <b>triangulation</b>
+ * : commencez par un retour en dur, puis généralisez dès qu'un deuxième test vous y force.
  *
- * <p>
- * Règles : pour chaque entier {@code n},
+ * <p>Règles : pour chaque entier {@code n},
  *
  * <ul>
- * <li>si {@code n} est multiple de 3 <em>et</em> de 5, retourner
- * {@code "FizzBuzz"}
- * <li>sinon si {@code n} est multiple de 3, retourner {@code "Fizz"}
- * <li>sinon si {@code n} est multiple de 5, retourner {@code "Buzz"}
- * <li>sinon retourner la représentation textuelle de {@code n}
+ *   <li>si {@code n} est multiple de 3 <em>et</em> de 5, retourner {@code "FizzBuzz"}
+ *   <li>sinon si {@code n} est multiple de 3, retourner {@code "Fizz"}
+ *   <li>sinon si {@code n} est multiple de 5, retourner {@code "Buzz"}
+ *   <li>sinon retourner la représentation textuelle de {@code n}
  * </ul>
  */
 public class FizzBuzzer {
@@ -51,15 +46,14 @@ public class FizzBuzzer {
    * @param n taille de la séquence demandée (positif)
    * @return tableau de {@code n} chaînes FizzBuzz dans l'ordre croissant
    */
-  public String[] fizzBuzzJusqua(int n) {
+  public String[] fizzBuzzJusquA(int n) {
 
     String[] sequence = new String[n];
 
     for (int i = 0; i < n; i++) {
-        sequence[i] = fizzBuzz(i + 1);
+      sequence[i] = fizzBuzz(i + 1);
     }
 
     return sequence;
-}
   }
 }
